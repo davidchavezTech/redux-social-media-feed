9e54348e92ffef05f0234dfc4cfc5e81c00d1929
David Chavez <davidsaso1234@gmail.com>
2021-08-24 10:23:22 -0500

    fixed edit post, can now react with emojis buttons.  Created selector functions in the slice file for easy reducer edits.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Navbar } from './app/Navbar.jsx'
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm.jsx'
 
 function App() {
   return (
@@ -26,7 +27,8 @@ function App() {
               </>
             )}
           />
-		  <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,20 +3,27 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
+import { ReactionButtons } from './ReactionButtons';
+import { selectAllPosts } from './postsSlice'
 export const PostsList = () => {
-    const posts = useSelector(state => state.posts)
+    const posts = useSelector(selectAllPosts)
 
-    const renderedPosts = posts.map(post => (
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    const renderedPosts = orderedPosts.map(post => {
+        return(
         <article className="post-excerpt" key={post.id} id={post.id}>
             <h3>{post.title}</h3>
             <p className="post-content">{post.content.substring(0,100)}</p>
+            <ReactionButtons post={post} />
             <PostAuthor userId={post.user} /><br />
             <TimeAgo timestamp={post.date} /><br />
             <Link to={`/posts/${post.id}`} className="button muted-button">
                 View Post
             </Link>
         </article>
-    ))
+        )
+    })
     return (
         <section className="posts-list">
             <h2>Posts</h2>
